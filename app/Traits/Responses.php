13eeb1e2a1a6024f6638503b48a698d472e20f8f
abdelra7man <abdelra7manabdullah@gmail.com>
2023-12-15 09:27:55 +0200

    adding success and error methods in responses class

--- app/Traits/Responses.php
+++ app/Traits/Responses.php
@@ -1,1 +1,16 @@
 <?php
+class Responses {
+    public static function success($message, $data = null, $statusCode = 200) {
+        return response()->json([
+            'message' => $message,
+            'statusCode' => $statusCode,
+            'data' => $data,
+        ], $statusCode);
+    }
+    public static function error($message, $statusCode = 400) {
+        return response()->json([
+            'message' => $message,
+            'statusCode' => $statusCode,
+        ], $statusCode);
+    }
+}
