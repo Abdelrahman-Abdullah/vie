c2840222829b69c5c579e58ced18792350a9988b
abdelra7man <abdelra7manabdullah@gmail.com>
2023-12-20 18:28:16 +0200

    add save code method to service and add logic code of forget controller with add mailtrip cred.

--- app/Http/Controllers/API/ForgetPasswordController.php
+++ app/Http/Controllers/API/ForgetPasswordController.php
@@ -9,17 +9,32 @@ use App\Notifications\ResetPasswordCodeNotification;
 use App\Services\ResetPasswordService;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\Notification;
+use Throwable;
 
 class ForgetPasswordController extends Controller
 {
-    public function __construct(Public ResetPasswordService $resetPasswordService){}
+    public function __construct(public ResetPasswordService $resetPasswordService)
+    {
+    }
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(UserResetPasswordRequest $request)
     {
-        // Generate Reset Code
-        // Send Email
-        // Return Response
+        try {
+            //Generate Reset Code
+            $resetCode = $this->resetPasswordService->getResetCode();
+            //Save Reset Code
+            $this->resetPasswordService->saveResetCode($request->validated('email'), $resetCode);
+            // Send Email
+            Notification::route('mail', $request->validated('email'))
+                ->notify(new ResetPasswordCodeNotification($resetCode));
+            // Return Response
+            return response()->json(['message' => 'Reset Code Sent Successfully']);
+        } catch (Throwable $th) {
+            return response()->json(['message' => 'Something went wrong']);
+        }
+
     }
 }
--- app/Services/ResetPasswordService.php
+++ app/Services/ResetPasswordService.php
@@ -1,11 +1,19 @@
 <?php
 namespace App\Services;
+use App\Models\ResetPassword;
 use Illuminate\Support\Str;
 
 class ResetPasswordService
 {
-    public function getResetCode(int $length = 6): int
+    public function getResetCode(int $length = 6): string
     {
         return Str::random($length);
+    }
+    public function saveResetCode(string $email, string $code): void
+    {
+        ResetPassword::create([
+            'email' => $email,
+            'code' => $code,
+        ]);
     }
 }
