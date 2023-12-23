ec0b3ba6ff324152948c20b037de8c77c7c78f42
abdelra7man <abdelra7manabdullah@gmail.com>
2023-12-23 13:07:04 +0200

    fix checkPasswordResetCodeController get rid of delete code out of the condation

--- app/Http/Controllers/API/CheckPasswordResetCodeController.php
+++ app/Http/Controllers/API/CheckPasswordResetCodeController.php
@@ -21,9 +21,9 @@ class CheckPasswordResetCodeController extends Controller
         $isCodeExist = $this->resetPasswordService->isCodeExist($code);
         $isCodeExpired = $this->resetPasswordService->isCodeExpired($code);
         if ($isCodeExist && !$isCodeExpired) {
-            $isCodeExist->delete();
             return response()->json(['message' => 'Code Is Correct']);
         }
+        $isCodeExist->delete();
         return response()->json(['message' => 'Code Was Expired Please Try Another One..'], 400);
     }
 }
--- app/Http/Controllers/API/ResetPasswordController.php
+++ app/Http/Controllers/API/ResetPasswordController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\ResetPasswordRequest;
 use App\Services\ResetPasswordService;
 use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
     public function __construct(public ResetPasswordService $resetPasswordService){}
-    public function __invoke(Request $request)
+    public function __invoke(ResetPasswordRequest $request)
     {
 
     }
