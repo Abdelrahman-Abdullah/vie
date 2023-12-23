03c5db2f7bae2e9694fb3bbe5b1dfc038784b03e
abdelra7man <abdelra7manabdullah@gmail.com>
2023-12-23 13:02:29 +0200

    add reset password request with update structre of reset controller request

--- app/Http/Controllers/API/ResetPasswordController.php
+++ app/Http/Controllers/API/ResetPasswordController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\ResetPasswordService;
 use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
-    //
+    public function __construct(public ResetPasswordService $resetPasswordService){}
+    public function __invoke(Request $request)
+    {
+
+    }
 }
