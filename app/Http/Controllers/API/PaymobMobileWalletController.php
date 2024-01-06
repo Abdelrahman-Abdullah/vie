664be38ff1e35280c59dd7c3a1475874f55510dd
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-06 20:19:12 +0200

    add post request in mobile wallet controller to get the final frame

--- app/Http/Controllers/API/PaymobMobileWalletController.php
+++ app/Http/Controllers/API/PaymobMobileWalletController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\PayOrderRequest;
 use App\Services\PaymobWalletService;
 use App\Traits\Wallet;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
 
 class PaymobMobileWalletController extends Controller
@@ -14,7 +15,30 @@ class PaymobMobileWalletController extends Controller
 
     public function __construct(protected PaymobWalletService $paymobWalletService){}
 
+    public function __invoke(PayOrderRequest $request): JsonResponse
+    {
+        $payment_token = $this->getAuthToken()
+            ->orderRegistration($request)
+            ->getPaymentKey();
 
+        try {
+            $response = Http::withHeaders(
+                $this->paymobWalletService->getUnAuthRequestHeader()
+            )->post($this->base_url . 'acceptance/payments/pay',[
+                'source' => [
+                    'identifier' => $request->validated('phone_number'),
+                    'subtype' => 'WALLET',
+                ],
+                'payment_token' => $payment_token,
+            ]);
+            return $response->json()['redirect_url'];
+        }catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
+
+    }
 
 
 }
