640bbea9d6f842c11729f94fdb8d6c6ca7c13420
abdelra7man <abdelra7manabdullah@gmail.com>
2024-01-07 20:20:54 +0200

    update the order table and add cast enum in order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Enum\OrderStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'status' => OrderStatus::class,
+    ];
 }
